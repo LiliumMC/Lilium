eaa51cc97a65ca7f0bd34c39763d49f5d3a409d1
Romonov <Romonov2003@outlook.com>
2019-06-12 13:54:27 +0800

    浅墨是日志大神（确信）

--- Lilium/Program.cs
+++ Lilium/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using log4net.Repository;
+using System;
+using System.IO;
 
 namespace Lilium
 {
     class Program
     {
+        internal static ILoggerRepository repository = null;
+        internal static ILog log = null;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            repository = LogManager.CreateRepository("Lilium");
+            XmlConfigurator.ConfigureAndWatch(repository, new FileInfo("log4net.config"));
+            log = LogManager.GetLogger(repository.Name, "Lilium");
+
         }
     }
 }
